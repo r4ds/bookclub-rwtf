12da068e5dc1d78a37bd58930c0ba6a9de2510f1
Federica Gazzelloni <61802414+Fgazzelloni@users.noreply.github.com>
2022-12-15 19:34:01 +0100

    Chapter2 (#9)
    * chapter2
    
    * updating description
    
    * updating chapter2
    
    * Don't library rstudioapi.
    
    Since the book builder doesn't have rstudio.
    
    * Don't call rstudioapi.
    
    Co-authored-by: Jon Harmon <jonthegeek@gmail.com>

---
+++ scripts/2_example1.R
@@ -0,0 +1,9 @@
+# Chapter 2 Example 1
+
+ex1 <- function(n) {
+  x = rnorm(n)
+  plot(x)
+}
+
+
+ex1(100)
---
+++ scripts/2_example2.R
@@ -0,0 +1,11 @@
+# Chapter 2 Example 2
+
+# this file is saved in a folder named *scripts* and I am attempting sourcing
+# it without specifying my wd
+source("2_example1.R")
+
+getwd()
+setwd("/Users/macintoshhd/Documents/R/R_general_resources/Bookclubs/bookclub-rwtf/scripts")
+source("2_example1.R")
+
+
